b3081cd6fca8c925f49773f69646406b2399abcb
ochienghesbon <ochienghesbon@student.moringaschool.com>
2022-06-13 15:23:03 +0300

    commit tree b8f8cadf9668516819bf26dfdfc0ca85134ef557

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    const p=document.querySelector('#text');
+    p.innerHTML = "This is really cool!";
+  });
+//   document.addEventListener("DOMContentLoaded", function() {
+//     let newLine = document.getElementById("text");
+//     newLine.textContent = "This is really cool!"
+// });
